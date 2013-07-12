35e75cfc420d13665bda8e26b0cfd8859b9a39c7
Ziul <ziuloliveira@gmail.com>
2013-07-12 09:17:26 -0300

    Revisão do estouro de memoria

--- DeadReckoning2/src/SensorCor.java
+++ DeadReckoning2/src/SensorCor.java
@@ -2,19 +2,35 @@ import lejos.nxt.ColorSensor;
 import lejos.nxt.LCD;
 import lejos.nxt.SensorPort;
 
-public class SensorCor implements Runnable {
 
-	// public SensorCor(MovimentacaoBasica movimento){
-	// this.movimento = movimento;
-	// }
-
+public class SensorCor implements Runnable{
+	
+//	public SensorCor(MovimentacaoBasica movimento){
+//		this.movimento = movimento;
+//	}
+	
+	
+	
 	@Override
 	public void run() {
 		ColorSensor cs = new ColorSensor(SensorPort.S3);
-		while (true) {
+		while(true){
 			ColorSensor.Color vals = cs.getColor();
-			LCD.drawString("Color: " + vals.getColor(), 0, 6);
-			// LCD.asyncRefresh();
+			StringBuffer read =new StringBuffer( "Color: "+vals.getColor());
+			LCD.drawString(read.substring(0), 0, 6);
+			if(vals.getColor()==7){
+				Robo.movimento.pararInstantaneamente();
+				Robo.movimento.moverParaFrente(7);
+				
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			else{
+			}
+			read.delete(0, read.length());
 		}
 	}
 
--- DeadReckoning2/src/SensorSonico.java
+++ DeadReckoning2/src/SensorSonico.java
@@ -4,26 +4,33 @@ import lejos.nxt.UltrasonicSensor;
 
 public class SensorSonico extends Thread {
 
-	private int read=0; 
 	public SensorSonico() {
 	}
 
+
+	/*
+	 * Thread para leitura do sensor sonico caso haja algum obstaculo o robo
+	 * fara um retangulo de 30 cm de largura e 50 cm de altura para contorna-lo
+	 * e voltar� a posi��o inicial(eu espero)
+	 */
 	public void run() {
 		UltrasonicSensor sonico = new UltrasonicSensor(SensorPort.S4);
 
 		while (true) {
 			sonico.continuous();
-			this.read = sonico.getDistance();
-			if (this.read !=0) {
+			// instancia
+			StringBuffer read =new StringBuffer( ""+sonico.getDistance());
 				try {
 					Thread.sleep(100);
-					LCD.drawString(""+this.read,0,5);
+					LCD.drawString(read.substring(0) ,0,0);
 				} catch (InterruptedException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-			}
-		}
+			// deleta instancia
+			read.delete(0, read.length());
+
+		}	
 	}
 
 }
